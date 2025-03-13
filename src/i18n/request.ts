7c87fb30e93edc9703792013d5c2e4c23daf1ded
jl75 <jan.lubbers@smartbits-consulting.nl>
2025-03-13 13:05:27 +0100

    bug: minimal change to demonstrate bug

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,6 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
-import {hasLocale} from 'next-intl';
+import {hasLocale, IntlErrorCode} from 'next-intl';
 
 export default getRequestConfig(async ({requestLocale}) => {
   // Typically corresponds to the `[locale]` segment
@@ -10,6 +10,13 @@ export default getRequestConfig(async ({requestLocale}) => {
     : routing.defaultLocale;
 
   return {
+    // @@BUG: This will not compile - else omitted for brevity
+    onError(error) {
+      if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+        // Missing translations are expected and should only log an error
+        console.error(error);
+      }
+    },
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default
   };
